8309325b4f31ebd2fe6f787a3b5389712ac8ee3f
k2works <kakimomokuri@gmail.com>
2014-05-28 19:55:49 +0900

    apt-get updateがうまく実行されない

--- myapp/cookbooks/mycookbook/recipes/default.rb
+++ myapp/cookbooks/mycookbook/recipes/default.rb
@@ -1,8 +1,22 @@
 #
-# Cookbook Name:: 
+# Cookbook Name::
 # Recipe:: default
 #
-# Copyright (C) 2014 
+# Copyright (C) 2014
 #
-# 
 #
+#
+bash 'bootstrap' do
+  code <<-EOC
+    echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+  EOC
+end
+
+execute "apt-get-update-periodic" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
